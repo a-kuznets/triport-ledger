a8a5da43d3ee215fd874394b63fc81413d98b18c
Alex Kuznetsov <akuznets96@gmail.com>
2023-04-28 21:45:25 -0400

    Add types for bot commands and Triport errors.

--- src/discord/bot.ts
+++ src/discord/bot.ts
@@ -2,11 +2,13 @@ import { readdirSync } from 'fs';
 import path from 'path';
 import { Client, Collection, Intents } from 'discord.js';
 import config from '../../config/discord.json' assert { type: 'json' };
+import { Command } from './command.js';
+import TriportError from '../triport/error.js';
 
-const commandsPath = path.resolve('src/discord/commands');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.commands = new Collection();
+const commandsPath = path.resolve('src/discord/commands');
+const commands = new Collection<string, Command>();
 const commandFiles = readdirSync(commandsPath).filter(file => {
 	return file.endsWith('.js')
 });
@@ -14,7 +16,7 @@ const commandFiles = readdirSync(commandsPath).filter(file => {
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = await import(filePath);
-	client.commands.set(command.data.name, command);
+	commands.set(command.data.name, command);
 }
 
 client.once('ready', () => {
@@ -23,16 +25,16 @@ client.once('ready', () => {
 
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
-	const command = client.commands.get(interaction.commandName);
+	const command = commands.get(interaction.commandName);
 	if (!command) return;
 
 	try {
 		const response = await command.execute(interaction);
 		await interaction.reply({ content: response });
 	} catch (error) {
-		if (error.name === 'TriportError') {
+		if (error instanceof TriportError) {
 			await interaction.reply({ content: error.message });
-		} else {
+		  } else {
 			console.error(error);
 		}
 	}
--- src/triport/error.ts
+++ src/triport/error.ts
@@ -1,6 +1,5 @@
 export default class TriportError extends Error {
-    constructor(message) {
+    constructor(message: string) {
         super(message);
-        this.name = 'TriportError';
     }
 }
